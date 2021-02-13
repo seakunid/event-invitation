7b1d877649297fb6fcf1cf0873686215a0389349
lubisarieff <57782182+lubisarieff@users.noreply.github.com>
2021-02-13 16:16:42 +0700

    add get data event and user

---
+++ src/event/data/index.js
@@ -0,0 +1,32 @@
+const credentials = require('../../../client_secret.json');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const docsDataEventSeakun = new GoogleSpreadsheet('1IRNy2D4l1-E0uzdaUWYt2IKBtd7ALlXI1Gmaaqx8Qx8');
+
+const getEvent = async (eventName) => {
+	let event = {};
+	await docsDataEventSeakun.useServiceAccountAuth({
+		client_email: credentials.client_email,
+		private_key: credentials.private_key,
+	});
+
+	await docsDataEventSeakun.loadInfo();
+	const sheet = docsDataEventSeakun.sheetsByIndex[0];
+	const rows = await sheet.getRows();
+	if (rows) {
+		for (let i = 0; i < rows.length; i++) {
+			if (rows[i]._rawData[4] === eventName) {
+				event = {
+					type_event: rows[i]._rawData[2],
+					title_event: rows[i]._rawData[3],
+					link_event: rows[i]._rawData[4],
+					date: rows[i]._rawData[8],
+					time: rows[i]._rawData[9],
+				};
+				break;
+			}
+		}
+	}
+	return event;
+};
+
+module.exports = getEvent;
---
+++ src/event/user/index.js
@@ -0,0 +1,31 @@
+const credentials = require('../../../client_secret.json');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const docsDataUserEventSeakun = new GoogleSpreadsheet('1sy0iFHLW4T6TtrERBjocXBR4g4M1cIpo_rFy5vvFy0A');
+
+const getUserEvent = async (eventName) => {
+	let users = [];
+	await docsDataUserEventSeakun.useServiceAccountAuth({
+		client_email: credentials.client_email,
+		private_key: credentials.private_key,
+	});
+
+	await docsDataUserEventSeakun.loadInfo();
+	const sheet = await docsDataUserEventSeakun.sheetsByIndex[0];
+	const rows = await sheet.getRows();
+	if (rows) {
+		rows.forEach((row) => {
+			if (row._rawData[1] === eventName) {
+				let user = {
+					fullname: row._rawData[3],
+					email: row._rawData[4],
+					whatsapp: row._rawData[5],
+				};
+
+				users.push(user);
+			}
+		});
+	}
+	return users;
+};
+
+module.exports = getUserEvent;
