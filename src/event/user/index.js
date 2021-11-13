6fdbc1cdede1dae627c3e19024c4a304516cd7de
lubisarieff <lubisarieff@gmail.com>
2021-11-13 07:50:28 +0700

    modified sheet user and get users from start to end

--- src/event/user/index.js
+++ src/event/user/index.js
@@ -1,31 +1,29 @@
 const credentials = require('../../../client_secret.json');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const docsDataUserEventSeakun = new GoogleSpreadsheet('1sy0iFHLW4T6TtrERBjocXBR4g4M1cIpo_rFy5vvFy0A');
+const docsDataUsers = new GoogleSpreadsheet('1bRXKTlboGMqSq8-ryBBLJVxoHSEvHf5kdoQnEPVcpGU');
 
-const getUserEvent = async (eventName) => {
-	let users = [];
-	await docsDataUserEventSeakun.useServiceAccountAuth({
+const getUsers = async (start, end) => {
+	const users = [];
+	await docsDataUsers.useServiceAccountAuth({
 		client_email: credentials.client_email,
 		private_key: credentials.private_key,
 	});
 
-	await docsDataUserEventSeakun.loadInfo();
-	const sheet = await docsDataUserEventSeakun.sheetsByIndex[1];
+	await docsDataUsers.loadInfo();
+	const sheet = await docsDataUsers.sheetsByIndex[0];
 	const rows = await sheet.getRows();
-	if (rows) {
-		rows.forEach((row) => {
-			if (row._rawData[1] === eventName) {
-				let user = {
-					fullname: row._rawData[3],
-					email: row._rawData[4],
-					whatsapp: row._rawData[5],
-				};
-
-				users.push(user);
-			}
-		});
+	if (!rows) {
+		return users;		
+	}
+	for (let i = start; i <= end; i++) {
+		const user = {
+			fullname: rows[i]._rawData[1],
+			email: rows[i]._rawData[2],
+			whatsapp: rows[i]._rawData[3],
+		}
+		users.push(user);
 	}
 	return users;
 };
 
-module.exports = getUserEvent;
+module.exports = getUsers;
